dc0e27360e150f1973c9d342972b3dc92e6854f9
Gina Fedder <ginafedder@yahoo.com>
2018-10-02 17:45:17 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,7 +19,7 @@ class Song
   end 
   
   def self.create_by_name(name)
-    self.new_by_name.tap{|song| song.save}
+    self.new_by_name(name).tap{|song| song.save}
   end 
   
   def self.find_by_name(name)
