3ef7dbb97634f32a843dd2a3cccb163f1696b8fa
Gina Fedder <ginafedder@yahoo.com>
2018-10-02 17:32:07 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,6 +11,36 @@ class Song
   end
   
   def self.create
-    self.new
+    self.new.tap{|song| song.save}
+  end 
+  
+  def self.new_by_name(name)
+    song = self.new 
+    song.name = name 
+    #self.new.tap{|song| song.name = name}
+  end 
+  
+  def self.create_by_name(name)
+    self.new_by_name.tap{|song| song.save}
+  end 
+  
+  def self.find_by_name(name)
+    self.all.find{|song| song.name == name}
+  end 
+  
+  def self.find_or_create_by_name(name)
+    if song = self.find_by_name(name)
+      song
+    else 
+      self.create_by_name(name)
+    end 
+  end
+  
+  def self.alphabetical
+    self.all.sort_by{|song| song.name}
+  end 
+  
+  def self.new_from_filename(file)
+    artist_name, file_name = file.split(" _ ")
 
 end
